c763f3b716832e2c495b9fe70942903c8e2e5165
limdabeen <ldb74777@naver.com>
2025-10-14 16:37:04 -0400

    feat: Initial commit with Spring Boot project setup

---
+++ src/main/java/example/nyc/NycApplication.java
@@ -0,0 +1,13 @@
+package example.nyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NycApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NycApplication.class, args);
+    }
+
+}
---
+++ src/test/java/example/nyc/NycApplicationTests.java
@@ -0,0 +1,13 @@
+package example.nyc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NycApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
